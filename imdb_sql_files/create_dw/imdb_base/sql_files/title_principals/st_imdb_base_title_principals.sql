e3432b0d422c5e880e1d64fa0050443e88192c65
Kevin K <kevin.l.kimm@gmail.com>
2021-01-11 23:02:55 -0500

    NEW SAs

---
+++ imdb_sql_files/create_dw/imdb_base/sql_files/title_principals/st_imdb_base_title_principals.sql
@@ -0,0 +1,19 @@
+/* DROP STAGING TABLE (ST) */
+DROP TABLE IF EXISTS ST_IMDB_BASE_TITLE_PRINCIPALS;
+
+/* CREATE ST TABLE */
+CREATE TABLE ST_IMDB_BASE_TITLE_PRINCIPALS (
+	TCONST_NK TEXT	-- alphanumeric unique identifier of the title
+	,ORDERING INT -- a number to uniquely identify rows for a given titleId
+	,NCONST TEXT --alphanumeric unique identifier of the name/person
+	,CATEGORY TEXT --the category of job that person was in
+	,JOB TEXT --the specific job title if applicable, else '\N'
+	,CHARACTERS TEXT --the name of the character played if applicable, else '\N'
+	,SRC_ID TEXT NOT NULL
+	,LOAD_DATE DATE NOT NULL
+	,NOTE TEXT NOT NULL
+	,CREATED_AT TIMESTAMPTZ NOT NULL DEFAULT NOW()
+  ,PRIMARY KEY (
+		TCONST_NK
+		)
+	);
