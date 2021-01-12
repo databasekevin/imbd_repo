a4d757d81f25265724403779da90f0a1372587f3
Kevin <kevin.l.kimm@gmail.com>
2021-01-12 15:39:45 -0500

    name basiscs updates

---
+++ imdb_sql_files/create_dw/imdb_base/sql_files/name_basics/ext_imdb_base_name_basics.sql
@@ -0,0 +1,12 @@
+/* CREATE AND LOAD ETRACT TABLE (EXT) */
+DROP TABLE IF EXISTS EXT_IMDB_BASE_NAME_BASICS;
+
+	/* TABLE NAME AND COLUMNS SHOULD MATCH SOURCE */
+	CREATE TABLE EXT_IMDB_BASE_NAME_BASICS (
+		TCONST TEXT	--(string) - alphanumeric unique identifier of the name/person
+		,PRIMARYNAME  TEXT	--(string)– name by which the person is most often credited
+		,BIRTHYEAR  TEXT	--in YYYY format
+		,DEATHYEAR  TEXT	--in YYYY format if applicable, else '\N'
+		,PRIMARYPROFESSION   TEXT	--(array of strings)– the top-3 professions of the person
+		,KNOWNFORTITLES   TEXT --(array of tconsts) – titles the person is known for
+		);
---
+++ imdb_sql_files/create_dw/imdb_base/sql_files/name_basics/st_imdb_base_name_basics.sql
@@ -0,0 +1,19 @@
+/* DROP STAGING TABLE (ST) */
+DROP TABLE IF EXISTS ST_IMDB_BASE_NAME_BASICS;
+
+/* CREATE ST TABLE */
+CREATE TABLE ST_IMDB_BASE_NAME_BASICS (
+	TCONST_NK TEXT NOT NULL	--(string) - alphanumeric unique identifier of the name/person
+	,PRIMARYNAME  TEXT NOT NULL	--(string)– name by which the person is most often credited
+	,BIRTHYEAR  TEXT NOT NULL	--in YYYY format
+	,DEATHYEAR  TEXT NOT NULL	--in YYYY format if applicable, else '\N'
+	,PRIMARYPROFESSION   TEXT NOT NULL	--(array of strings)– the top-3 professions of the person
+	,KNOWNFORTITLES   TEXT NOT NULL --(array of tconsts) – titles the person is known for
+	,SRC_ID TEXT NOT NULL
+	,LOAD_DATE DATE NOT NULL
+	,NOTE TEXT NOT NULL
+	,CREATED_AT TIMESTAMPTZ NOT NULL DEFAULT NOW()
+  ,PRIMARY KEY (
+		TCONST_NK
+		)
+	);
